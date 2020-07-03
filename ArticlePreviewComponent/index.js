9c0eccef590a7f1ff77425d80e3d01c958eac2f7
JakubK <jakubkuniszewski1@gmail.com>
2020-07-03 14:50:50 +0200

    Add active class to article footer too

--- ArticlePreviewComponent/index.js
+++ ArticlePreviewComponent/index.js
@@ -6,6 +6,9 @@ const shareBtn = document.querySelector('.article__share');
 //find share block
 const share = document.querySelector('.share');
 
+//find share footer
+const shareFooter = document.querySelector('.article__footer');
+
 //toggling
 shareBtn.addEventListener('click', function()
 {
@@ -13,7 +16,13 @@ shareBtn.addEventListener('click', function()
 
   share.style.display = shareToggled ? 'flex' : 'none';
   if(shareToggled)
+  {
     shareBtn.classList.add('active');
+    shareFooter.classList.add('active');
+  }
   else
+  {
     shareBtn.classList.remove('active');
+    shareFooter.classList.remove('active');
+  }
 });
